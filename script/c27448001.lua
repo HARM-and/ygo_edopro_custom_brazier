2d8b802e38061842778d3cb4e3212f817ab49cfe
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 10:45:33 +0200

    Delete c27448001.lua

--- script/c27448001.lua
+++
@@ -1,8 +0,0 @@
---Larbin du Brazier
-function c27488001.initial_effect(c)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EFFECT_DRAW_COUNT)
-	e1:SetValue(5)
-	c:RegisterEffect(e1)
-end
