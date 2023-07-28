cac1ee216361d8346e5d3cc68773ac1d4bda8fe7
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 11:41:21 +0200

    Update c511027001.lua

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -2,12 +2,13 @@
 function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DRAW)
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c511027001.target)
 	e1:SetOperation(c511027001.activate)
 	c:RegisterEffect(e1)
+	
 end
 
 function c511027001.target(e,tp,eg,ep,ev,re,r,rp,chk)
