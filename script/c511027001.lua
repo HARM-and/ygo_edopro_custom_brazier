3bab23ff287667ea379878867b057e5c2726d71b
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 10:54:39 +0200

    Add files via upload

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -1,9 +1,9 @@
 --Larbin du Brazier
 function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EFFECT_DRAW_COUNT)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(2)
+	e1:SetValue(1000)
 	c:RegisterEffect(e1)
 end
