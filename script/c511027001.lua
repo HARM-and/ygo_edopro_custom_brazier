b17e6ff2b385ea77f27def4409527bfa2e31a580
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 11:28:52 +0200

    Update c511027001.lua

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -1,10 +1,9 @@
 --Larbin du Brazier
 function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetCode(EFFECT_DRAW_COUNT)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
