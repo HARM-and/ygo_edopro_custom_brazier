cccc73e89fcaf1240a11b71d9c689c5daf7cb93a
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 11:39:45 +0200

    Add files via upload

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -1,10 +1,31 @@
 --Larbin du Brazier
 function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCode(EFFECT_DRAW_COUNT)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(1,0)
-	e1:SetValue(1)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c511027001.target)
+	e1:SetOperation(c511027001.activate)
 	c:RegisterEffect(e1)
 end
+
+function c511027001.target(e,tp,eg,ep,ev,re,r,rp,chk)
+
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	
+	Duel.SetTargetPlayer(tp)
+	
+	Duel.SetTargetParam(2)
+	
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	
+	end
+	
+	function c511027001.activate(e,tp,eg,ep,ev,re,r,rp)
+	
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	
+	Duel.Draw(p,d,REASON_EFFECT)
+	
+	end
