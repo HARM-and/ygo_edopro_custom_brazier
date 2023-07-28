0422ed73d76e2cc0a8f1dc7ea73e00619ebe6d1d
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 14:01:56 +0200

    Update c511027002.lua

--- script/c511027002.lua
+++ script/c511027002.lua
@@ -5,7 +5,7 @@ function c511027002.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c511027002.thtg)
+	e1:SetTarget(c511027002.toHandFromDeck)
 	e1:SetOperation(c511027002.thop)
 	c:RegisterEffect(e1)
 end
@@ -13,10 +13,17 @@ end
 function c511027002.filter1(c)
 	return c:IsSetCard(0xb3a) and c:IsAbleToHand()
 end
+
+function c511027002.toHandFromDeck(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c511027002.filter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
 function c511027002.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c511027002.filter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
+
 function c511027002.thop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
