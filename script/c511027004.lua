4a8768d666717ac0fe010ae91a5aad5fde7000ea
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 22:02:59 +0200

    aze

--- script/c511027004.lua
+++ script/c511027004.lua
@@ -1,8 +1,6 @@
 --Forêt Embrazier
-local s,id=GetID()
 function c511027004.initial_effect(c)
     c:EnableCounterPermit(0xb3c)
-
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
@@ -17,8 +15,8 @@ end
 c511027004.counter_list={COUNTER_SPELL}
 
 function c511027004.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsCanRemoveCounter(tp,0xb3c,0,COUNTER_SPELL,3,REASON_COST) end
-	Duel.RemoveCounter(tp,0xb3c,0,COUNTER_SPELL,3,REASON_COST)
+	if chk==0 then return Duel.IsCanRemoveCounter(tp,0xb3c,1,REASON_COST) end
+	Duel.RemoveCounter(tp,0xb3c,1,REASON_EFFECT)
 end
 
 function c511027004.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -28,6 +26,35 @@ function c511027004.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function c511027004.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+end
+
+--メガトン魔導キャノン
+--Mega Ton Magical Cannon
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.counter_list={COUNTER_SPELL}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsCanRemoveCounter(tp,1,0,COUNTER_SPELL,10,REASON_COST) end
+	Duel.RemoveCounter(tp,1,0,COUNTER_SPELL,10,REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
 	Duel.Destroy(g,REASON_EFFECT)
 end
