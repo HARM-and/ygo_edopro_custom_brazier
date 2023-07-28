b736f276ac2a9e99704b61684fe88a220bbe9e45
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 18:36:23 +0200

    Update c511027004.lua

--- script/c511027004.lua
+++ script/c511027004.lua
@@ -1,11 +1,3 @@
 --Forêt Embrazier
 function c511027004.initial_effect(c)
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c511027004.target)
-	e1:SetOperation(c511027004.activate)
-	c:RegisterEffect(e1)
 end
