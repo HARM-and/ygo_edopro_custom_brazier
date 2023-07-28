8971bd1e14ec2485c56c5e30a65f08d14b1e4282
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 13:56:01 +0200

    Switch repositories

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -2,7 +2,7 @@
 function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DRAW)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c511027001.target)
@@ -11,13 +11,21 @@ function c511027001.initial_effect(c)
 end
 
 function c511027001.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(2)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
-end
 
-function c511027001.activate(e,tp,eg,ep,ev,re,r,rp)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	
+	Duel.SetTargetPlayer(tp)
+	
+	Duel.SetTargetParam(2)
+	
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	
+	end
+	
+	function c511027001.activate(e,tp,eg,ep,ev,re,r,rp)
+	
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	
 	Duel.Draw(p,d,REASON_EFFECT)
-end
+	
+	end
---
+++ script/c511027002.lua
@@ -0,0 +1,28 @@
+--Garde du Brazier
+function c511027002.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c511027002.thtg)
+	e1:SetOperation(c511027002.thop)
+	c:RegisterEffect(e1)
+end
+
+function c511027002.filter1(c)
+	return c:IsSetCard(0xb3a) and c:IsAbleToHand()
+end
+function c511027002.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c511027002.filter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function c511027002.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c511027002.filter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
