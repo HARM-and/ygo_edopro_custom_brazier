67f50ed08b820b1260d8e28d0ce8e1afea9753cc
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 10:57:47 +0200

    Update c511027001.lua

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -1,7 +1,7 @@
 --Larbin du Brazier
 function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetType(EFFECT_TYPE_ACTIONS)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(8000)
