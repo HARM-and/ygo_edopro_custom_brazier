607500f71002340b78f997a42d4a298d652b8455
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 11:11:25 +0200

    Update c511027001.lua

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -4,6 +4,8 @@ function c511027001.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetCode(EFFECT_DRAW_COUNT)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 end
