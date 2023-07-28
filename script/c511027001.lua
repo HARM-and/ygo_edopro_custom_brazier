2b62ae233a534b9627ff45712385e549674b6b1c
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-28 10:52:35 +0200

    Add files via upload

--- script/c511027001.lua
+++ script/c511027001.lua
@@ -3,6 +3,7 @@ function c511027001.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EFFECT_DRAW_COUNT)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(2)
 	c:RegisterEffect(e1)
 end
