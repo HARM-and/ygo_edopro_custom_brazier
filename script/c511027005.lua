f57dba9feaa8a27de76fe5d14aa63c0f525869f1
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-30 21:42:58 +0200

    Update c511027005.lua

--- script/c511027005.lua
+++ script/c511027005.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xb3a))
-	e2:SetValue(aux.TargetBoolFunction(Card.IsRace,RACE_PYRO))
+	e2:SetValue(s.indct)
 	c:RegisterEffect(e2)
 
 	--Add counter
@@ -24,7 +24,7 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetRange(LOCATION_FZONE)
-	e3:SetTarget(s.onlyPyro)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_PYRO))
 	e3:SetOperation(s.op)
 	c:RegisterEffect(e3)
 end
