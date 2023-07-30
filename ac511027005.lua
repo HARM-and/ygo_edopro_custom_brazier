a0411e7fec65555366199dc7bf5d1b4e45fc7000
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-30 15:10:07 +0200

    azeaze

--- ac511027005.lua
+++ ac511027005.lua
@@ -1,6 +1,7 @@
 --Grand Brazier
 function c511027005.initial_effect(c)
 	c:EnableCounterPermit(0xb3c)
+
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -17,40 +18,16 @@ function c511027005.initial_effect(c)
 	c:RegisterEffect(e3)
 
 	--Remove counter replace
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(511027005,0))
-	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e4:SetCode(EFFECT_RCOUNTER_REPLACE+0xb3c)
-	e4:SetRange(LOCATION_FZONE)
-	e4:SetCountLimit(1)
-	e4:SetCondition(c511027005.rcon)
-	e4:SetOperation(c511027005.rop)
-	c:RegisterEffect(e4)
+	--local e4=Effect.CreateEffect(c)
+	--e4:SetDescription(aux.Stringid(511027005,0))
+	--e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	--e4:SetCode(EFFECT_REPL)
+	--e4:SetRange(LOCATION_SZONE)
+	--e4:SetCountLimit(1)
+	--e4:SetCondition(c511027005.rcon)
+	--e4:SetOperation(c511027005.rop)
+	--c:RegisterEffect(e4)
 
-	--Destroy replace
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e5:SetCode(EFFECT_DESTROY_REPLACE)
-	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e5:SetRange(LOCATION_FZONE)
-	e5:SetTarget(c511027005.desreptg)
-	e5:SetOperation(c511027005.desrepop)
-	c:RegisterEffect(e5)
-
-	--Add counter2
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e6:SetCode(EVENT_LEAVE_FIELD_P)
-	e6:SetRange(LOCATION_FZONE)
-	e6:SetOperation(c511027005.addop2)
-	c:RegisterEffect(e6)
-end
-
-function c511027005.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=re:GetHandler()
-	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and c~=e:GetHandler() then
-		e:GetHandler():AddCounter(0xb3c,1)
-	end
 end
 
 function c511027005.rcon(e,tp,eg,ep,ev,re,r,rp)
@@ -59,6 +36,13 @@ end
 
 function c511027005.rop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():RemoveCounter(ep,0xb3c,ev,REASON_EFFECT)
+end
+
+function c511027005.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=re:GetHandler()
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and c~=e:GetHandler() then
+		e:GetHandler():AddCounter(0xb3c,1)
+	end
 end
 
 function c511027005.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
--- script/c511027005.lua
+++ script/c511027005.lua
@@ -1,6 +1,8 @@
---Grand Brazier
-function c511027005.initial_effect(c)
-	c:EnableCounterPermit(0xb3c)
+--ウィッチクラフト・バイストリート
+--Witchcrafter Bystreet
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
 
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -8,63 +10,23 @@ function c511027005.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
 
-	--Add counter
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e3:SetCode(EVENT_CHAIN_SOLVING)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetOperation(c511027005.op)
-	c:RegisterEffect(e3)
-
-	--Remove counter replace
-	--local e4=Effect.CreateEffect(c)
-	--e4:SetDescription(aux.Stringid(511027005,0))
-	--e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	--e4:SetCode(EFFECT_REPL)
-	--e4:SetRange(LOCATION_SZONE)
-	--e4:SetCountLimit(1)
-	--e4:SetCondition(c511027005.rcon)
-	--e4:SetOperation(c511027005.rop)
-	--c:RegisterEffect(e4)
-
+	--indestructable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xb3a))
+	e2:SetValue(s.indct)
+	c:RegisterEffect(e2)
 end
 
-function c511027005.rcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsActivated() and bit.band(r,REASON_COST)~=0 and ep==e:GetOwnerPlayer() and e:GetHandler():GetCounter(0xb3c)>=ev
-end
+s.listed_series={0xb3a}
 
-function c511027005.rop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RemoveCounter(ep,0xb3c,ev,REASON_EFFECT)
-end
-
-function c511027005.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=re:GetHandler()
-	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and c~=e:GetHandler() then
-		e:GetHandler():AddCounter(0xb3c,1)
-	end
-end
-
-function c511027005.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsReason(REASON_RULE+REASON_REPLACE)
-		and e:GetHandler():IsCanRemoveCounter(tp,0xb3c,1,REASON_EFFECT) end
-	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
-end
-
-function c511027005.desrepop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RemoveCounter(tp,0xb3c,1,REASON_EFFECT)
-end
-
-function c511027005.addop2(e,tp,eg,ep,ev,re,r,rp)
-	local count=0
-	local c=eg:GetFirst()
-	while c~=nil do
-		if c~=e:GetHandler() and c:IsOnField() and c:IsReason(REASON_DESTROY) then
-			count=count+c:GetCounter(0xb3c)
-		end
-		c=eg:GetNext()
-	end
-	if count>0 then
-		e:GetHandler():AddCounter(0xb3c,count)
+function s.indct(e,re,r,rp)
+	if (r&REASON_BATTLE+REASON_EFFECT)~=0 then
+		return 1
+	else
+		return 0
 	end
 end
