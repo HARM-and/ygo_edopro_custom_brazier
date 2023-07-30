23008c888a6c7424bd125d7404da22fc8aee3382
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-30 19:48:39 +0200

    Update c511027005.lua

--- script/c511027005.lua
+++ script/c511027005.lua
@@ -24,13 +24,21 @@ function c511027005.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetRange(LOCATION_FZONE)
-	e3:SetTarget(aux.TargetBoolFunction(c:IsRace(RACE_PYRO)))
+	e3:SetTarget(c.filter)
 	e3:SetTargetRange(LOCATION_MZONE,0)
 	e3:SetOperation(c511027005.op)
 	c:RegisterEffect(e3)
 end
 
 c511027005.listed_series={0xb3a}
+
+function c511027005.filter(c)
+    return c:IsRace(RACE_PYRO)
+end
+
+function c511027005.thtg(c)
+    return c:IsRace(RACE_PYRO)
+end
 
 function c511027005.indct(e,re,r,rp)
 	if (r&REASON_BATTLE+REASON_EFFECT)~=0 then
