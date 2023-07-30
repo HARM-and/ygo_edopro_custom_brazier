4a4885668cd5e248a461429608661e2ba31e51a4
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-30 19:18:43 +0200

    Update c511027005.lua

--- script/c511027005.lua
+++ script/c511027005.lua
@@ -24,6 +24,7 @@ function c511027005.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
 	e3:SetOperation(c511027005.op)
 	c:RegisterEffect(e3)
 end
@@ -39,6 +40,6 @@ function c511027005.indct(e,re,r,rp)
 end
 
 function c511027005.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=re:GetHandler()
+	local c=eg:GetHandler()
 	e:GetHandler():AddCounter(0xb3c,1)
 end
