0ba83f1b56c6dac2d78570f4eae68697ad26b52c
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-07-31 15:05:00 +0200

    Update c511027005.lua

--- script/c511027005.lua
+++ script/c511027005.lua
@@ -87,7 +87,6 @@ end
 --Add counter to Ritual monster before discard
 function s.tcop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
-	local count=e:GetHandler():GetCounter(0xb3c)
-	tc:AddCounter(0xb3c,count)
+	tc:AddCounter(0xb3c,e:GetHandler():GetCounter(0xb3c))
 	Duel.SendtoGrave(e:GetHandler(), REASON_EFFECT)
 end
